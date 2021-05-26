197ba6347a0f86d7a8fe59aacb7e51335f9cec48
川辺 兼嗣 <kawanabe_kenji@grenge.co.jp>
2021-05-27 01:30:24 +0900

    モデルの向きに対応

--- Assets/BlurObject/BlurObject.cs
+++ Assets/BlurObject/BlurObject.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class BlurObject : MonoBehaviour
 {
+    /// <summary> BlurシェーダーのプロパティID </summary>
     private static readonly int PROPERTY_TRAIL_DIR = Shader.PropertyToID("_TrailDir");
 
     [SerializeField]
@@ -12,21 +13,29 @@ public class BlurObject : MonoBehaviour
     private Vector3 _trailPos;
 
     [SerializeField]
+    private Transform _rotationRoot;
+
+    /// <summary> 残像の尻尾の追従スピード </summary>
+    [SerializeField]
     private float _trailRate = 10f;
 
-    void Awake()
+    private void Awake()
     {
+        // materialにアクセスして、複製されたマテリアルを変数に入れる
         _material = _renderer.material;
         _trailPos = transform.position;
     }
 
-    void Update()
+    private void Update()
     {
         _trailPos = Vector3.Lerp(_trailPos, transform.position, Mathf.Clamp01(Time.deltaTime * _trailRate));
-        _material.SetVector(PROPERTY_TRAIL_DIR, _trailPos - transform.position);
+        Transform root = _rotationRoot != null ? _rotationRoot : transform;
+        // オブジェクトの回転を考慮してローカル方向に変換する
+        Vector3 dir = root.InverseTransformDirection(_trailPos - transform.position);
+        _material.SetVector(PROPERTY_TRAIL_DIR, dir);
     }
 
-    void OnDrawGizmosSelected()
+    private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.cyan;
         Gizmos.DrawSphere(_trailPos, 0.2f);
